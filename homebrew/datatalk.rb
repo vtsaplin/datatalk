48e7eba542d42632cb577db3cfbc6fb7d34e2a4b
Vitaly Tsaplin <tsaplin@adobe.com>
2025-09-02 01:18:26 +0200

    Fix corrupted Homebrew formula
    - Restore proper file format
    - Maintain correct SHA256 hash: c0aa7399ce942e9b2dda12ebd1e4de1557cd039e705e3a39784d48f23cc1fea6

--- homebrew/datatalk.rb
+++ homebrew/datatalk.rb
@@ -1,12 +1,8 @@
-cla  desc "Query CSV and Parquet data with natural language"
-  homepage "https://github.com/vtsaplin/datatalk"
-  url "https://github.com/vtsaplin/datatalk/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c0aa7399ce942e9b2dda12ebd1e4de1557cd039e705e3a39784d48f23cc1fea6"
-  license "MIT"tatalk < Formula
+class Datatalk < Formula
   include Language::Python::Virtualenv
 
   desc "Query CSV and Parquet data with natural language"
-  homepage "https://github.com/tsaplin/datatalk"
+  homepage "https://github.com/vtsaplin/datatalk"
   url "https://github.com/vtsaplin/datatalk/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "c0aa7399ce942e9b2dda12ebd1e4de1557cd039e705e3a39784d48f23cc1fea6"
   license "MIT"
