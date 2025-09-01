605411c0fd5f9cf19a5635e96a3d90de771e8de2
Vitaly Tsaplin <tsaplin@adobe.com>
2025-09-02 01:15:36 +0200

    Update Homebrew formula to v0.1.0
    - Add correct SHA256 hash: c0aa7399ce942e9b2dda12ebd1e4de1557cd039e705e3a39784d48f23cc1fea6
    - Update URL to point to actual GitHub release tarball

--- homebrew/datatalk.rb
+++ homebrew/datatalk.rb
@@ -1,14 +1,14 @@
 cla  desc "Query CSV and Parquet data with natural language"
   homepage "https://github.com/vtsaplin/datatalk"
   url "https://github.com/vtsaplin/datatalk/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "c0aa7399ce942e9b2dda12ebd1e4de1557cd039e705e3a39784d48f23cc1fea6"
   license "MIT"tatalk < Formula
   include Language::Python::Virtualenv
 
   desc "Query CSV and Parquet data with natural language"
   homepage "https://github.com/tsaplin/datatalk"
-  url "https://github.com/tsaplin/datatalk/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256_WILL_BE_UPDATED_BY_RELEASE_SCRIPT"
+  url "https://github.com/vtsaplin/datatalk/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "c0aa7399ce942e9b2dda12ebd1e4de1557cd039e705e3a39784d48f23cc1fea6"
   license "MIT"
   head "https://github.com/vtsaplin/datatalk.git", branch: "main"
 
