3beee5e8a249bdac33cb43e57a40e150c15ce7e0
Vitaly Tsaplin <tsaplin@adobe.com>
2025-09-02 01:35:48 +0200

    Update Homebrew formula for consistency with PyPI
    - Rename formula class to DatatalkCli
    - Use PyPI as source instead of GitHub tarball
    - Update SHA256 for PyPI package: b8b1ec9d68e1cd08305021ba6097c01893cca33a6fe044e38a72e3b192d3553f
    - Rename file to datatalk-cli.rb for consistency

--- homebrew/datatalk-cli.rb
+++ homebrew/datatalk-cli.rb
@@ -1,10 +1,10 @@
-class Datatalk < Formula
+class DatatalkCli < Formula
   include Language::Python::Virtualenv
 
   desc "Query CSV and Parquet data with natural language"
   homepage "https://github.com/vtsaplin/datatalk"
-  url "https://github.com/vtsaplin/datatalk/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c0aa7399ce942e9b2dda12ebd1e4de1557cd039e705e3a39784d48f23cc1fea6"
+  url "https://files.pythonhosted.org/packages/source/d/datatalk-cli/datatalk_cli-0.1.0.tar.gz"
+  sha256 "b8b1ec9d68e1cd08305021ba6097c01893cca33a6fe044e38a72e3b192d3553f"
   license "MIT"
   head "https://github.com/vtsaplin/datatalk.git", branch: "main"
 
